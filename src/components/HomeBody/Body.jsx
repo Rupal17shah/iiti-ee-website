517403821e74607e45ed3d5e6ebf88eafd75fd32
Ghost4681 <64606908+Ghost4681@users.noreply.github.com>
2023-02-09 00:39:01 +0530

    added tailwind, framer motion, animations are done for the homepage

--- src/components/HomeBody/Body.jsx
+++ src/components/HomeBody/Body.jsx
@@ -1,14 +1,22 @@
 import { Box } from '@mui/system'
 import React from 'react'
 import AboutUs from './AboutUs'
+import { motion } from "framer-motion";
 import Events from './Events'
 const Body = () => {
   return (
     <>
-    <Box sx={{px:10}}>
-      <Events/>
-      <AboutUs/>
-    </Box>
+      <Box sx={{ px: 10 }}>
+        <motion.div
+          className="w-full"
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.3, type: "spring", stiffness: 100 }}
+        >
+          <Events />
+        </motion.div>
+        <AboutUs />
+      </Box>
 
 
     </>
